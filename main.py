dcd4de04d80f10383aee66db227cae716bdb598d
FengLing <m13998337244@163.com>
2022-04-20 01:48:35 +0800

    refactor with Codeforces api

--- main.py
+++ main.py
@@ -1,48 +1,27 @@
-from bs4.element import Tag
-from bs4 import BeautifulSoup
 import requests
 import time
 import pandas as pd
 
 
-# Get page count of contests
-html = requests.get("https://codeforces.com/contests/")
-soap = BeautifulSoup(html.content, 'lxml')
-pages = soap.select('.page-index > a')
-max_page_num = int(pages[-1].get_text())
+# Get data by Codeforces api
+contests = requests.get("https://codeforces.com/api/contest.list?gym=false").json()['result']
 
 
-# Get a list of all contests
-base_url = "https://codeforces.com/contests/page/"
-contests_list = []
-
-for page_num in range(1, max_page_num + 1):
-    time.sleep(0.2)
-    url = base_url + str(page_num)
-    print('running... ' + str(page_num) + '/' + str(max_page_num))
-    html = requests.get(url)
-    soap = BeautifulSoup(html.content, 'lxml')
-    contests = soap.select('#pageContent > div.contestList > div.contests-table > div.datatable > div:nth-child(6) > '
-                           'table > tr[data-contestid]')
-    for contest in contests:  # type: Tag
-        cells = contest.select('td')
-        name_cell = cells[0]  # type: Tag
-        time_cell = cells[2]  # type: Tag
-        contest_name = name_cell.contents[0].strip()  # type: str
-        link = 'https://codeforces.com' + name_cell.a['href']  # type: str
-        start_time = time_cell.span.get_text().strip()  # type: str
-        if 'Educational' in contest_name:
-            contests_list.append({
-                'contest_name': contest_name,
-                'link': link,
-                'start_time': start_time
-            })
+# Deal with contests list.
+# Erase non-educational round.
+contests = list(filter(lambda x: 'Educational' in x['name'], contests))
+# Reverse contests list to sort by time in ascending order.
+contests.reverse()
 
 
-# Process result
-pf = pd.DataFrame(list(contests_list))
-order = ['contest_name', 'link', 'start_time']
-pf = pf[order]
-file_path = pd.ExcelWriter('results.xlsx')
-pf.to_excel(file_path, encoding='utf-8', index=False)
-file_path.save()
+# Format contests. (Add attributes about link and format time)
+for contest in contests:
+    contest['link'] = 'https://codeforces.com/contest/' + str(contest['id'])
+    contest['formatTime'] = time.strftime("%Y-%m-%d %H:%M", time.localtime(contest['startTimeSeconds']))
+
+
+# Process result.
+result = pd.DataFrame(list(contests))
+order = ['name', 'link', 'formatTime']
+result = result[order]
+result.to_excel('results.xlsx', encoding='utf-8', index=False)
